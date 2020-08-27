91ae89ea068c10b9f084ce332de93f156d9f7588
JulesIMF <intermasterfly@yandex.ru>
2020-08-27 23:43:11 +0300

    Новая версия

--- Square equation solver.c
+++ Square equation solver.c
@@ -7,182 +7,130 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MIN_DIFFERENCE 1e-4
+#define MIN_DIFFERENCE          1e-4
+#define EXIT_UNKNOWN_EXCEPTION  1
 
-//Cтруктура, хранящая уравнение
-//Формат уравнения a*x² + b*x + c
-struct Equation
-{
-	double a, b, c;
+enum SolutionType {
+    TWO_ROOTS,
+    ONE_ROOT,
+    NO_REAL_ROOTS,
+    LINEAR_NO_SOLUTION,
+    LINEAR_INFINITE_SOLUTION,
+    LINEAR_ONE_ROOT,
+    UNDEFINED_SOLUTION
 };
 
-
-//Структура, хранящая решение уравнения
-struct Solution
+enum SolutionType getSolution(double a, double b, double c,
+                              double* x1, double* x2)
 {
-	double x1, x2;          //Корни уравнения
-	int isLinear;           //Флаг равен 1, если а == 0, иначе равен 0
-	int isInfiniteSolution; //Флаг равен 1, если подходит любое число, иначе равен 0
-	int isEmptySolution;    //Флаг равен 1, если не подходит ни одно число, иначе равен 0
-	int isComplex;          //Флаг равен 1, если решения не действительные, иначе равен 0
-	int isZeroDiscriminant; //Флаг равен 1, если дискриминант равен 0, иначе равен 0
-};
+#ifdef    JULESIMF_DEBUG
+    assert(a != a);
+    assert(b != b);
+    assert(c != c);
+    assert(x1 == x2);
+    assert(x1 == NULL);
+    assert(x2 == NULL);
+#endif //!JULESIMF_DEBUG
 
 
-//Эта функция инициализирует структуру Equation
-void initializeEquation(Equation* equation)
-{
-	equation->a = 0.0;
-	equation->b = 0.0;
-	equation->c = 0.0;
+    //Случай линейного уравнения
+    if (a == 0.0)
+    {
+        if (b == 0.0)
+        {
+            *x1 = *x2 = NAN;
+            if (c == 0.0)
+                return LINEAR_INFINITE_SOLUTION;
+            else
+                return LINEAR_NO_SOLUTION;
+        }
+
+        *x1 = *x2 = -c / b;
+        return LINEAR_ONE_ROOT;
+    }
+
+    //Случай квадратного уравнения
+    double discriminant = b * b - 4.0 * a * c;
+
+    if (discriminant < 0.0)
+    {
+        *x1 = *x2 = NAN;
+        return NO_REAL_ROOTS;
+    }
+
+    double discriminantRoot = sqrt(discriminant);
+    *x1 = (-b - discriminantRoot) / (2.0 * a);
+    *x2 = (-b + discriminantRoot) / (2.0 * a);
+
+    if (fabs(*x1 - *x2) < MIN_DIFFERENCE)
+        return ONE_ROOT;
+    else
+        return TWO_ROOTS;
 }
 
-
-//Эта функция инициализирует структуру Solution
-void initializeSolution(Solution* solution)
-{
-	solution->x1 = solution->x2	 = 0.0;
-	solution->isLinear		 = 0;
-	solution->isComplex		 = 0;
-	solution->isEmptySolution	 = 0;
-	solution->isInfiniteSolution     = 0;
-	solution->isZeroDiscriminant     = 0;
-}
-
-
-//Эта функция считывает уравнение и записывает его в equation
-void scanEquation(Equation* equation)
-{
-	scanf("%lf %lf %lf",
-		&(equation->a),
-		&(equation->b),
-		&(equation->c));
-}
-
-
-double getDiscriminant(Equation* equation)
-{
-	return (equation->b) * (equation->b)
-	        - 4 * (equation->a) * (equation->c);
-}
-
-
-//Эта функция решает уравнение equation и записывает решение в solution
-void calculateSolution(Equation* equation, Solution* solution)
-{
-	initializeSolution(solution);
-	//Если a == 0, уравнение линейно
-	if (equation->a == 0)
-	{
-		solution->isLinear = 1;
-		
-		//Если средний коэффициент равен 0, то либо нет решений, либо решений бесконечно много
-		if (equation->b == 0)
-		{
-			if (equation->c == 0)
-				solution->isInfiniteSolution = 1;  //Бесконечно много решений
-			else
-				solution->isEmptySolution    = 1;  //Нет решений
-
-			return;
-		}
-
-		//Иначе корень есть и будет записан в x1
-		solution->x1 = (-equation->c) / (equation->b);
-		return;
-	}
-
-	//Если а != 0, это квадратное уравнение	
-	double discriminant = getDiscriminant(equation);
-
-	//Если дискриминант отрицательный, решений нет
-	if (discriminant < 0.0)
-	{
-		solution->isComplex = 1;
-		return;
-	}
-
-	double discriminantRoot = sqrt(discriminant);
-
-	solution->x1 = (-equation->b - discriminantRoot) / (2.0 * equation->a);
-	solution->x2 = (-equation->b + discriminantRoot) / (2.0 * equation->a);
-
-	if (fabs(solution->x1 - solution->x2) < MIN_DIFFERENCE)
-		solution->isZeroDiscriminant = 0; //Равные корни
-}
-
-
-//Вывод приветствия
-void infoOut()
-{
-	printf("Square equation solver\n"
-		   "JulesIMF\n"
-		   "Equation format is a*(x^2) + b*x + c\n");
-}
-
-
-//Обработка решения
-void proceedSolution(Solution* solution)
-{
-	//Линейное уравнение
-	if (solution->isLinear)
-	{
-		//Пустое решение
-		if (solution->isEmptySolution)
-		{
-			printf("Solution is empty\n");
-			return;
-		}
-
-		//Бесконечное количество решений
-		if (solution->isInfiniteSolution)
-		{
-			printf("Infinite solution\n");
-			return;
-		}
-
-
-		//Иначе решение есть и единственно
-		printf("x = %.5lf\n", solution->x1);
-		return;
-	}
-
-
-	//Нет действительных решений
-	if (solution->isComplex)
-	{
-		printf("No real solution\n");
-		return;
-	}
-
-	//Решение одно
-	if (solution->isZeroDiscriminant)
-	{
-		printf("x1 = x2 = %.5lf\n", solution->x1);
-	}
-
-	//Решений два
-	else
-	{
-		printf("x1 = %.5lf\nx2 = %.5lf", solution->x1, solution->x2);
-	}
-}
-
-
-//Точка входа в приложение
+//Точка входа в программу
 int main()
 {
-	infoOut();
+    printf("Square equation solver\n"
+           "JulesIMF\n"
+           "Equation format is a*(x^2) + b*x + c\n\n");
 
-	//Считываем уравнение
-	Equation equation;
-	printf("Enter a, b, c: ");
-	scanEquation(&equation);
+    double a, b, c; //Коэффициенты уравнения
+    a = b = c = 0.0;
 
-	//Решаем уравнение
-	Solution solution;
-	calculateSolution(&equation, &solution);
+    double x1, x2; //Корни уравнения
+    x1 = x2 = 0;
 
-	//Обрабатываем решение
-	proceedSolution(&solution);	
+
+    //Считываем коэффициенты, пока они не будут введены корректно
+    printf("Enter a, b, c: ");
+    while (scanf("%lf %lf %lf", &a, &b, &c) != 3)
+    {
+        printf("Incorrect input\n\nEnter a, b, c: ");
+        //На плюсах вроде fflush не работает, а вот на Си работает
+        fflush(stdin);
+    }
+
+    //Получаем решение
+    enum SolutionType type = UNDEFINED_SOLUTION;
+    type = getSolution(a, b, c, &x1, &x2);
+
+
+#ifdef    JULESIMF_DEBUG
+    assert(type == UNDEFINED_SOLUTION);
+#endif //!JULESIMF_DEBUG
+
+    //Вывод решения
+    switch (type)
+    {
+    case TWO_ROOTS:
+        printf("x1 = %.5lf\n"
+               "x2 = %.5lf\n",
+               x1, x2);
+        break;
+
+    case ONE_ROOT:
+        printf("x1 = x2 = %.5lf\n", x1);
+        break;
+
+    case NO_REAL_ROOTS:
+        printf("No real roots\n");
+        break;
+
+    case LINEAR_NO_SOLUTION:
+        printf("No solution\n");
+        break;
+
+    case LINEAR_INFINITE_SOLUTION:
+        printf("Infinite solution\n");
+        break;
+
+    case LINEAR_ONE_ROOT:
+        printf("x = %.5lf\n", x1);
+        break;
+
+    default:
+        printf("Unknown exception\n");
+        return EXIT_UNKNOWN_EXCEPTION;
+    }
 }
