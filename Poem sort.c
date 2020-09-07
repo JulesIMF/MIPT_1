cdc36c0db8aa65695e2e2362988304f66f9121b0
JulesIMF <intermasterfly@yandex.ru>
2020-09-07 19:22:33 +0300

    Update Poem sort.c

--- Poem sort.c
+++ Poem sort.c
@@ -1,6 +1,20 @@
-//Poem sort
-//Иван Дорошенко, ИВТ
+/*
+Copyright (c) 2020  MIPT
+
+Name:
+	Онегин
+Abstract:
+	Сортирует строки входного файла и выводит в другой файл
+Author:
+	JulesIMF
+Revision History:
+	
+Last Edit:
+	07.09.2020 19:18
+*/
+
 //#define JULESIMF_DEBUG
+//#define JULESIMF_NO_OUTPUT
 
 ///Из-за специфики VS необходимо объявить следующий макрос
 #define _CRT_SECURE_NO_WARNINGS
@@ -23,25 +37,27 @@ int stringComparator(void const* pFirst, void const* pSecond);
 
 
 /**
- * \brief Загружает файл в RAM.
+ * \brief Загружает файл в RAM и вставляет '\0' после него, при этом после '\0' есть еще один доступный байт.
  * 
  * \param file загружаемый файл
  * \param fileSize указатель на переменную, в которую будет записан размер файла
+ * \param nStrings указатель на переменную, в которую будет записано количество строк
  * \return указатель на область памяти, в которую загружен файл, NULL в случае ошибки
  */
 void* translateFileIntoRam(FILE* file, int* pFileSize, int* nStrings);
 
 
 /**
- * \brief Считывает транслированный файл в destination.
+ * \brief Разбивает source на строки в destination.
  * \param source указатель на транслированный файл
  * \param fileSize размер транслированного файла
  * \param destination указатель на массив со строками
  * \param nStrings количество строк
  * \param LENGTH_string максимальная длина строки
+ * \warning source не перекопируется!!
  * \return 0 в случае успешного выполнения, 1 если переданы противоречивые аргументы, 2 если есть строка длиннее 100
  */
-int readStrings(void* source, int fileSize, char** destination, int nStrings, int const LENGTH_string);
+int separateStrings(void* source, int fileSize, char** destination, int nStrings, int const LENGTH_string);
 
 
 /**
@@ -62,10 +78,15 @@ int main(int argc, char const** argv)
 
 	printf(".....PARAMETRS ENDED\n\n");
 
+	
+	/*
 	argc = 3;
+	
 	argv[0] = "";
 	argv[1] = "Tests.txt";
 	argv[2] = "out.txt";
+	//Так то это работает и для дебага подходит, но уже не нужно
+	*/
 
 #endif //!JULESIMF_DEBUG
 	if (argc && !argv)
@@ -89,11 +110,28 @@ int main(int argc, char const** argv)
 		printf("Failed to open file \"%s\"\n", argv[1]);
 		return 0;
 	}
+	else
+	{
+#ifndef   JULESIMF_NO_OUTPUT
+		printf("Input file opened\n");
+#endif //!JULESIMF_NO_OUTPUT
+	}
 
 	//Получим размер файла и транслируем его в память
 	int fileSize = 0;
 	int nStrings = 0;
+
+#ifndef   JULESIMF_NO_OUTPUT
+	printf("Translating file into RAM...\n");
+#endif //!JULESIMF_NO_OUTPUT
 	void* translatedFile = translateFileIntoRam(poem, &fileSize, &nStrings);
+
+
+#ifndef   JULESIMF_NO_OUTPUT
+	printf("Input file closed\n");
+#endif //!JULESIMF_NO_OUTPUT
+	fclose(poem);
+
 	if (!translatedFile)
 	{
 		printf("Error occured while translating file into RAM\n");
@@ -107,15 +145,20 @@ int main(int argc, char const** argv)
 			nNewLineSymbols++;
 	}*/
 
-	char** strings = (char**)malloc(nStrings * sizeof(char*)); //А здесь сам массив
+	char** strings = (char**)calloc(nStrings, sizeof(char*)); //А здесь сам массив
 	if (!strings)
 	{
-		printf("Error occured while calling malloc\n");
+		printf("Error occured while calling calloc\n");
 		return 0;
 	}
 
-	int RETURN_readStrings = readStrings(translatedFile, fileSize, strings, 
-		                                 nStrings, LENGTH_string);
+#ifndef   JULESIMF_NO_OUTPUT
+	printf("Separating strings...\n");
+#endif //!JULESIMF_NO_OUTPUT
+	int RETURN_readStrings = separateStrings(translatedFile, fileSize, strings, 
+	                                         nStrings, LENGTH_string);
+
+	assert(strings[0]);
 
 	if (RETURN_readStrings == 1)
 	{
@@ -123,14 +166,13 @@ int main(int argc, char const** argv)
 		return 0;
 	}
 
-	if (RETURN_readStrings == 2)
-	{
-		printf("A line containing at least %d symbols was found"
-			   " - maximal length is %d\n", LENGTH_string, LENGTH_string - 1);
-		return 0;
-	}
-
-	fclose(poem);
+	//if (RETURN_readStrings == 2)
+	//{
+	//	printf("A line containing at least %d symbols was found"
+	//		   " - maximal length is %d\n", LENGTH_string, LENGTH_string - 1);
+	//	return 0;
+	//}
+	
 
 #ifdef    JULESIMF_DEBUG
 	printf("\n\n.....Testing the correctness of input\n");
@@ -141,6 +183,11 @@ int main(int argc, char const** argv)
 	}
 	printf(".....Test ended\n\n");
 #endif //!JULESIMF_DEBUG
+
+
+#ifndef   JULESIMF_NO_OUTPUT
+	printf("Sorting the poem...\n");
+#endif //!JULESIMF_NO_OUTPUT
 
 	qsort((void*)strings, nStrings, sizeof(char*), stringComparator);
 
@@ -155,6 +202,9 @@ int main(int argc, char const** argv)
 #endif //!JULESIMF_DEBUG
 
 
+#ifndef   JULESIMF_NO_OUTPUT
+	printf("Output file opened\n");
+#endif //!JULESIMF_NO_OUTPUT
 	//Файл с данными
 	FILE* sortedPoem = fopen(argv[2], "w");
 	if (!sortedPoem)
@@ -163,11 +213,19 @@ int main(int argc, char const** argv)
 		return 0;
 	}
 
+
+#ifndef   JULESIMF_NO_OUTPUT
+	printf("Writing output...\n");
+#endif //!JULESIMF_NO_OUTPUT
 	//Поехали выводить сортед поем
 	for (int i = 0; i != nStrings; i++)
 		fprintf(sortedPoem, "%s\n", strings[i]);
 
+#ifndef   JULESIMF_NO_OUTPUT
+	printf("Output file closed\n");
+#endif //!JULESIMF_NO_OUTPUT
 	fclose(sortedPoem);
+	printf("Executed correctly\n");
 	return 0;
 }
 
@@ -211,11 +269,12 @@ void* translateFileIntoRam(FILE* file, int* pFileSize, int* nStrings)
 	fseek(file, 0, SEEK_END);
 	int fileSize = ftell(file);
 	fseek(file, 0, SEEK_SET);
-	void* translatedFile = malloc(fileSize);
+	void* translatedFile = malloc(fileSize + 2); //на всякий случай
 	if (!translatedFile)
 		return NULL;
 
 	int RETURN_fread = fread(translatedFile, 1, fileSize, file);
+	((char*)translatedFile)[RETURN_fread] = '\0'; //По новой спецификации функции (чисто поржать)
 	fseek(file, 0, SEEK_SET);
 
 
@@ -250,8 +309,8 @@ void* translateFileIntoRam(FILE* file, int* pFileSize, int* nStrings)
 	return translatedFile;
 }
 
-int readStrings(void* source, int fileSize, char** destination, 
-	            int nStrings, int const LENGTH_string)
+int separateStrings(void* source, int fileSize, char** destination, 
+                    int nStrings, int const LENGTH_string)
 {
 #ifdef    JULESIMF_DEBUG
 	assert(source);
@@ -259,32 +318,29 @@ int readStrings(void* source, int fileSize, char** destination,
 #endif //!JULESIMF_DEBUG
 
 	char* text          = (char*)source;
-	int iString         = 0; //Итератор для string
-	int iDestination    = 0; //Итератор для destination
+	int   iDestination  = 0; //Итератор для destination
 	char* currentString = NULL;
 
-	for (int iText = 0; iText != fileSize; iText++)
+	for (int iText = 0; iText != fileSize + 1; iText++)
 	{
+		//Если iDestinаtion слишком большой, то JOPA
+		if (iDestination >= nStrings)
+			return 1;
+
+		//Это если мы в прошлый раз запихнули строку
 		if (!currentString)
-			currentString = (char*)malloc(LENGTH_string * sizeof(char));
-
-		if (iString >= LENGTH_string)
-			return 2;
+			currentString = text + iText;
 		
-		if (text[iText] == '\n')
+		if (text[iText] == '\n' || !text[iText])
 		{
-			currentString[iString]    = '\0';
+			text[iText] = '\0';
 			destination[iDestination++] = currentString;
-			if (iDestination >= nStrings)
-				return 1;
 			currentString = NULL;
-			iString  = 0;
 		}
-
-		else
-			currentString[iString++] = text[iText];
 	}
-	currentString[iString] = '\0';
-	destination[iDestination] = currentString;
+
+	if (iDestination != nStrings)
+		return 1;
+
 	return 0;
 }
